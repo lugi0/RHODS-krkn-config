16ef13c0e374becc5f0f2fccf1b1bcbc57c2ffec
Luca Giorgi <lgiorgi@redhat.com>
2023-04-18 12:45:54 +0200

    Add base config files
    Signed-off-by: Luca Giorgi <lgiorgi@redhat.com>

---
+++ scenarios/rhods/post_action_pods_prometheus_modelmonitoring.py
@@ -0,0 +1,33 @@
+import subprocess
+import logging
+import time
+
+NAMESPACE= "redhat-ods-monitoring"
+LABEL= "app.kubernetes.io/instance=rhods-model-monitoring"
+CONTAINER_NO= "3"
+PODS_NO= "3"
+
+
+def run(cmd):
+    try:
+        output = subprocess.Popen(
+            cmd, shell=True, universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT
+        )
+        (out, err) = output.communicate()
+    except Exception as e:
+        logging.error("Failed to run %s, error: %s" % (cmd, e))
+    return out
+
+
+i = 0
+while i < 100:
+    pods_running = run(f"oc get pods -n {NAMESPACE} -l {LABEL} | grep -c '{CONTAINER_NO}/{CONTAINER_NO}'").rstrip()
+    if pods_running == PODS_NO:
+        break
+    time.sleep(5)
+    i += 1
+
+if pods_running == PODS_NO:
+    print(f"There were {PODS_NO} pods running properly")
+else:
+    print(f"ERROR there were {pods_running} pods running instead of {PODS_NO}")
